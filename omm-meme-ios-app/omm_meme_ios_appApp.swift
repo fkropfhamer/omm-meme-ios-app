d7c207d27fb8cc046438c4bf54f814cc351b7324
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-01-27 15:53:24 +0100

    Initial Commit

---
+++ omm-meme-ios-app/omm_meme_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  omm_meme_ios_appApp.swift
+//  omm-meme-ios-app
+//
+//  Created by Fabian Kropfhamer on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct omm_meme_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
