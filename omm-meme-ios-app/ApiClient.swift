d4b0756837bdf4f5edaf4385aa5683e26c152bf5
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-01-28 13:51:16 +0100

    changed image type to jpeg

--- omm-meme-ios-app/ApiClient.swift
+++ omm-meme-ios-app/ApiClient.swift
@@ -11,10 +11,10 @@ import Alamofire
 
 class ApiClient {
     public static func upload(image: UIImage) {
-        let imageData = image.pngData()
+        let imageData = image.jpegData(compressionQuality: 1.0)
         
         AF.upload(
-            multipartFormData: { multipartFormData in multipartFormData.append(imageData!, withName: "template", fileName: "file.png", mimeType: "image/png")}, to: "http://localhost:8000/api/v1/template",
+            multipartFormData: { multipartFormData in multipartFormData.append(imageData!, withName: "template", fileName: "image-from-iphone.jpeg", mimeType: "image/jpg")}, to: "http://localhost:8000/api/v1/template",
             method: .post
         ) .responseJSON { res in
             print(res)
